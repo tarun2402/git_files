373acb25fdd6837ee5d8cc01c5eecfb7313af74e
Tarun Patel <tarunpatelpt7103@gmail.com>
2021-08-11 09:43:19 +0530

    initial commit

---
+++ Bank.java
@@ -0,0 +1,46 @@
+package com.tarun;
+
+public class Bank {
+	
+	//ac number name balance name email and phone number
+	private String ac_number;
+	private String name;
+	private double balance;
+	private String email_id;
+	private String ph_number;
+	public String getAc_number() {
+		return ac_number;
+	}
+	public void setAc_number(String ac_number) {
+		this.ac_number = ac_number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getEmail_id() {
+		return email_id;
+	}
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+	public String getPh_number() {
+		return ph_number;
+	}
+	public void setPh_number(String ph_number) {
+		this.ph_number = ph_number;
+	}
+	
+	
+	
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.tarun;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Bank sbi = new Bank();
+		
+		sbi.setAc_number("12345");
+		sbi.setBalance(25);
+		sbi.setEmail_id("tarun.patel");
+		
+		
+
+	}
+
+}
